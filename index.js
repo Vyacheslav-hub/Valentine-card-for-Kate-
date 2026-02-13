c7d0b0ee7e7b7b06f231494ea7a94d291e41dc09
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-02-13 14:22:18 +0300

    добавил файл mp3, добавил интерфейс плеера в html, добавил логику плеера в index.js

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+const audio = document.getElementById('valentineAudio');
 const left = document.getElementById('leftHalf');
 const right = document.getElementById('rightHalf');
 
+function checkHearts() {
+    // Проверяем, открыты ли ОБЕ половинки
+    const isLeftOpen = left.classList.contains('is-open');
+    const isRightOpen = right.classList.contains('is-open');
+
+    if (isLeftOpen && isRightOpen) {
+        audio.play().catch(e => {
+            console.log("Браузер ждет клика для запуска музыки");
+        });
+    } else {
+        audio.pause();
+    }
+}
+
+// Слушатели событий на клик
 left.addEventListener('click', () => {
     left.classList.toggle('is-open');
+    checkHearts();
 });
 
 right.addEventListener('click', () => {
     right.classList.toggle('is-open');
+    checkHearts();
 });
